c5ef733765082324c96e128b1e740f9bf81000c2
pieromarini <piero.marini21@gmail.com>
2019-08-22 19:22:28 -0500

    Solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
-using namespace std;
+int main() {  
+  std::string s;
+  std::multimap<std::string, std::string> memo;
 
-int main() {
-	return EXIT_SUCCESS;
+  while (std::cin >> s, s != "#") {
+	std::string t{s};
+	std::transform(s.begin(), s.end(), t.begin(), tolower);
+	std::sort(t.begin(), t.end());
+	memo.insert({t, s});
+  }
+
+  std::vector<std::string> ananagrams;
+
+  for (auto& p : memo) {
+	if (memo.count(p.first) == 1)
+	  ananagrams.push_back(p.second);
+  }
+
+  std::sort(ananagrams.begin(), ananagrams.end());
+  for (auto& an : ananagrams) {
+	std::cout << an << '\n'; 
+  }
+
+  return 0;
 }
-
